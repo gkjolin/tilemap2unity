e6564e8a8399d372deebbbfbd3612afc697b6b72
SpaceG <gkjolin@gmail.com>
2017-12-12 10:46:57 +0800

    init
    init

---
+++ UnityTile2/Assets/Tiled2Unity/Scripts/Camerfallow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerfallow : MonoBehaviour
+{
+    public Transform Target;
+    public Camera myCamera;
+    public float smooth = 5F;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myCamera.orthographicSize = (Screen.height / 1000f) * 5;
+    }
+    private float m_fTime = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lerp = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * smooth);
+        transform.position = lerp + new Vector3(0, 0, -10);
+
+        if (transform.position == Target.position)
+        {
+            Debug.Log(Time.time);
+        }
+
+    }
+}
---
+++ UnityTile2/Assets/Tiled2Unity/Scripts/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerManager : MonoBehaviour
+{
+
+    Rigidbody2D rigibody;
+    Animator anim;
+    float speed = 4f;
+    // Use this for initialization
+    void Start()
+    {
+        rigibody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (movement != Vector2.zero)
+        {
+            rigibody.MovePosition(rigibody.position + movement * Time.deltaTime * speed);
+        }
+    }
+}
